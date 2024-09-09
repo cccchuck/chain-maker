a331763590ba0a2a9dee0242e333bc9d1110a1f7
Chuck <chuckgao0223@gmail.com>
2024-09-09 15:48:33 +0800

    feat: change run mode

--- src/strategy-manager/index.ts
+++ src/strategy-manager/index.ts
@@ -2,9 +2,8 @@ import { UniswapClient, dextools } from '@/apis'
 import { strategies } from '@/config/strategies'
 import { type Strategy } from '@/config/strategies/types'
 import { client, walletClient } from '@/rpc'
-import { OHLCV, Position, TokenInfo } from '@/types'
+import { Position, TokenInfo } from '@/types'
 import { logger } from '@/utils'
-import dayjs from 'dayjs'
 
 export class StrategyManager {
   static strategies: Strategy[] = strategies
@@ -12,22 +11,24 @@ export class StrategyManager {
   static async run() {
     const uniswapClient = new UniswapClient(client, walletClient)
     // const ohlcvMap = new Map<string, OHLCV[]>()
-    // Key = token address
+    // Key = strategy.name + token address
     const tokenInfoMap = new Map<string, TokenInfo>()
-    // Key = strategy.name + tokenInfo.pair
+    // Key = strategy.name + token address
     const positionMap = new Map<string, Position | null>()
 
-    for (const strategy of this.strategies) {
-      logger.info(`[${strategy.name}] Initializing Strategy`)
+    const runInterval = async (strategies: Strategy[]) => {
+      for (const strategy of strategies) {
+        const tokenInfoKey = `${strategy.name}-${strategy.address}`
+        let tokenInfo: TokenInfo | undefined = tokenInfoMap.get(tokenInfoKey)
+        if (!tokenInfo) {
+          logger.info(`[${strategy.name}] Initializing Strategy`)
+          logger.info(`[${strategy.name}] Get Token: ${strategy.address} Info`)
+          tokenInfo = await dextools.fetchTokenInfo(strategy.address)
+          tokenInfoMap.set(tokenInfoKey, tokenInfo)
+        }
 
-      logger.info(`[${strategy.name}] Get Token: ${strategy.address} Info`)
-      const tokenInfo = await dextools.fetchTokenInfo(strategy.address)
-      tokenInfoMap.set(strategy.address, tokenInfo)
-
-      logger.info(`[${strategy.name}] Start Interval, Interval: 5000ms`)
-      setInterval(async () => {
         const ohlcv = await dextools.fetchOHLCV(tokenInfo.pair)
-        const position = positionMap.get(`${strategy.name}-${tokenInfo.pair}`)
+        const position = positionMap.get(tokenInfoKey)
         if (!position) {
           const isEntry = await strategy.isEntry(ohlcv, tokenInfo)
           if (isEntry) {
@@ -39,34 +40,13 @@ export class StrategyManager {
             logger.info(`[${strategy.name}] [${tokenInfo.symbol}] Exit`)
           }
         }
-      }, 5000)
-
-      // logger.info(`[${strategy.name}] Get Token: ${strategy.address} OHLCV`)
-      // const ohlcv = await DataProvider.fetchOHLCV(tokenInfo.pair)
-      // ohlcvMap.set(tokenInfo.pair, ohlcv)
-
-      // logger.info(`[${strategy.name}] Start Interval, Interval: 5000ms`)
-      // setInterval(async () => {
-      //   const ohlcv = await DataProvider.fetchOHLCV(tokenInfo.pair)
-      //   ohlcvMap.set(tokenInfo.pair, ohlcv)
-      //   const [ts, open, high, low, close, volume] = ohlcv[0]
-      //   logger.info(
-      //     `[${strategy.name}] [${tokenInfo.symbol}] Time: ${dayjs(ts).format(
-      //       'YYYY-MM-DD HH:mm:ss'
-      //     )} OPEN: ${open} HIGH: ${high} LOW: ${low} CLOSE: ${close} VOLUME: ${volume}`
-      //   )
-      //   logger.info(
-      //     `[${strategy.name}] [${tokenInfo.symbol}] Latest Price: ${close}`
-      //   )
-      //   const position = positionMap.get(tokenInfo.pair)
-      //   if (position) {
-      //     // 是否需要平仓
-      //   } else {
-      //     // 是否需要开仓
-      //   }
-      // }, 5000)
-
-      // DataProvider.subscribe(tokenInfo.pair, strategy.run)
+      }
     }
+
+    await runInterval(strategies)
+
+    setInterval(async () => {
+      await runInterval(strategies)
+    }, 5000)
   }
 }
