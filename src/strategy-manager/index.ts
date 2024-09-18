69610c23b677d34bd5099b4a9bc9d13cecb4824b
Chuck <chuckgao0223@gmail.com>
2024-09-18 15:18:16 +0800

    feat: add redis

--- src/strategy-manager/index.ts
+++ src/strategy-manager/index.ts
@@ -2,8 +2,9 @@ import { UniswapClient, dextools, erc20 } from '@/apis'
 import { strategies } from '@/config/strategies'
 import { type Strategy } from '@/config/strategies/types'
 import { SWAP_ROUTER_2_ADDRESS } from '@/const'
+import { redisClient } from '@/db'
 import { client, walletClient } from '@/rpc'
-import { Position, TokenInfo } from '@/types'
+import { TokenInfo } from '@/types'
 import { logger } from '@/utils'
 import { Address, formatUnits } from 'viem'
 
@@ -13,22 +14,30 @@ export class StrategyManager {
   static strategies: Strategy[] = strategies
 
   static async run() {
+    while (!redisClient.isReady) {
+      logger.error('Redis Client is not ready')
+      await new Promise((resolve) => setTimeout(resolve, 1000))
+    }
+    logger.info('Redis Client is ready')
+
     const uniswapClient = new UniswapClient(client, walletClient)
-    // const ohlcvMap = new Map<string, OHLCV[]>()
-    // Key = strategy.name + token address
-    const tokenInfoMap = new Map<string, TokenInfo>()
-    // Key = strategy.name + token address
-    const positionMap = new Map<string, Position>()
 
     const runInterval = async (strategies: Strategy[]) => {
       for (const strategy of strategies) {
-        const tokenInfoKey = `${strategy.name}-${strategy.address}`
-        let tokenInfo: TokenInfo | undefined = tokenInfoMap.get(tokenInfoKey)
+        const tokenInfoKey = `tokenInfo-${strategy.name}-${strategy.address}`
+        const positionKey = `position-${strategy.name}-${strategy.address}`
+        let tokenInfo: TokenInfo | undefined = JSON.parse(
+          (await redisClient.get(tokenInfoKey)) || 'null'
+        )
+        const position = JSON.parse(
+          (await redisClient.get(positionKey)) || 'null'
+        )
         if (!tokenInfo) {
           logger.info(`[${strategy.name}] Initializing Strategy`)
           logger.info(`[${strategy.name}] Get Token: ${strategy.address} Info`)
           tokenInfo = await dextools.fetchTokenInfo(strategy.address)
-          tokenInfoMap.set(tokenInfoKey, tokenInfo)
+          // tokenInfoMap.set(tokenInfoKey, tokenInfo)
+          await redisClient.set(tokenInfoKey, JSON.stringify(tokenInfo))
 
           logger.info(`[${strategy.name}] [${tokenInfo.symbol}] Get Allowance`)
           const allowance = await erc20.getAllowance(
@@ -56,11 +65,9 @@ export class StrategyManager {
         }
 
         const ohlcv = await dextools.fetchOHLCV(tokenInfo.pair)
-        const position = positionMap.get(tokenInfoKey)
         if (!position) {
           const isEntry = await strategy.isEntry(ohlcv, tokenInfo)
           if (isEntry) {
-            logger.info(`[${strategy.name}] [${tokenInfo.symbol}] Entry`)
             const { hash } = (await uniswapClient.swapExactETHForTokens(
               tokenInfo.address as Address,
               tokenInfo.decimals,
@@ -75,11 +82,14 @@ export class StrategyManager {
                 ),
                 tokenInfo.decimals
               )
-              positionMap.set(tokenInfoKey, {
-                entryTime: ohlcv[0][0],
-                entryPrice: ohlcv[0][4],
-                entryVolume: entryVolume,
-              })
+              await redisClient.set(
+                positionKey,
+                JSON.stringify({
+                  entryTime: ohlcv[0][0],
+                  entryPrice: ohlcv[0][4],
+                  entryVolume: entryVolume,
+                })
+              )
               logger.info(
                 `[${strategy.name}] [${tokenInfo.symbol}] Entry Success`
               )
@@ -91,7 +101,6 @@ export class StrategyManager {
         } else {
           const isExit = await strategy.isExit(ohlcv, tokenInfo, position)
           if (isExit) {
-            logger.info(`[${strategy.name}] [${tokenInfo.symbol}] Exit`)
             const { hash } = (await uniswapClient.swapExactTokensForETH(
               tokenInfo.address as Address,
               tokenInfo.decimals,
@@ -99,7 +108,7 @@ export class StrategyManager {
               strategy.slippage
             )) || { hash: null }
             if (hash) {
-              positionMap.delete(tokenInfoKey)
+              await redisClient.del(positionKey)
               logger.info(
                 `[${strategy.name}] [${tokenInfo.symbol}] Exit Success`
               )
@@ -114,8 +123,10 @@ export class StrategyManager {
 
     await runInterval(strategies)
 
-    setInterval(async () => {
+    const interval = setInterval(async () => {
       await runInterval(strategies)
     }, 5000)
+
+    return () => clearInterval(interval)
   }
 }
