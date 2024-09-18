4bf522353706a5ee0cbe3ede9390c162642547f0
Chuck <chuckgao0223@gmail.com>
2024-09-18 15:23:13 +0800

    feat: add SIGINT handler

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
+import { redisClient } from './db'
 import { StrategyManager } from './strategy-manager'
 import { logger } from './utils'
 
 const main = async () => {
-  StrategyManager.run()
+  const stop = await StrategyManager.run()
+  process.on('SIGINT', async () => {
+    logger.info('Stopping...')
+    try {
+      stop()
+      await redisClient.quit()
+      logger.info('Stopped')
+      process.exit(0)
+    } catch (error) {
+      logger.error(error)
+      process.exit(-1)
+    }
+  })
 }
 
 main().catch((error) => {
